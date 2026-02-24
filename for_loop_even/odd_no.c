92c9cf09a87d2531f192537fb1609e46610e4fdd
ramgitbit <rambhawan111777@gmail.com>
2026-02-24 20:30:32 +0530

    Add C program to print even numbers up to a given limit

---
+++ for_loop_even/odd_no.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter");
+    scanf("%d", &n);
+    for(int i=0; i <= n; i+=2){
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
+// Print of Odd No
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     printf("enter");
+//     scanf("%d", &n);
+//     for(int i=1; i <= n; i+=2){
+//         printf("%d\n", i);
+//     }
+    
+//     return 0;
+// }
+// 
